dcfcf29e32cfcb8b213f337b1e278ff945a98616
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-17 08:22:27 +0100

    add new query

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,11 @@
-export default function HomePage() {
+import { getAllInfosResearcher } from "@/lib/db/action"
+
+export default async function HomePage() {
+    const result = await getAllInfosResearcher("11/2374");
+
     return (
-        <div>
-            WIP
-        </div>
+        <pre>
+            {JSON.stringify(result, null, 2)}
+        </pre>
     )
 }
