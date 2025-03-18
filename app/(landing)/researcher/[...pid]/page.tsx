040e9aab22197c92531a60597345fb29cdac69a9
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-18 06:29:23 +0100

    garde async

--- app/(landing)/researcher/[...pid]/page.tsx
+++ app/(landing)/researcher/[...pid]/page.tsx
@@ -1,24 +1,30 @@
-import { Suspense } from "react"
 import ResearcherInfo from "./researcher-info";
 import ResearcherInfoDisplay from "./researcher-info-display";
+import { Suspense } from "react";
 import ResearcherPublications from "./researcher-publications";
 import ResearcherPublicationsDisplay from "./researcher-publications-display";
 
-export default function Page({
-    searchParams,
-  }: {
-    searchParams: { resolvedPid?: string }
-  }) {
-    const pid = searchParams.resolvedPid || ""
-  
+export default async function Page({
+    params
+}: {
+    params: Promise<{
+        pid: string[]
+    }>
+}) {
+    // Attendre la résolution de params avant de l'utiliser
+    const resolvedParams = await params
+    const pid = resolvedParams.pid.join("/")
+
     return (
-      <>
-        <Suspense fallback={<ResearcherInfoDisplay />}>
-          <ResearcherInfo pid={pid} />
-        </Suspense>
-        <Suspense fallback={<ResearcherPublicationsDisplay />}>
-          <ResearcherPublications pid={pid} />
-        </Suspense>
-      </>
+        <main className="flex flex-col h-full w-full items-center justify-start p-4" >
+            <section className="w-full max-w-4xl space-y-4">
+                <Suspense fallback={<ResearcherInfoDisplay />}>
+                    <ResearcherInfo pid={pid} />
+                </Suspense>
+                <Suspense fallback={<ResearcherPublicationsDisplay />}>
+                    <ResearcherPublications pid={pid} />
+                </Suspense>
+            </section>
+        </main>
     )
-  }
+}
