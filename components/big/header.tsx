df5e687bae760ebb9ccd22a62d891cdce31cdae3
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-19 07:25:09 +0100

    put the search component to the right

--- components/big/header.tsx
+++ components/big/header.tsx
@@ -6,15 +6,15 @@ import Search from './search';
 
 export default function Header({
     fullWidth
-} : {
+}: {
     fullWidth?: boolean
 }) {
     return (
         <header className="border-b border-gray-200 dark:border-gray-700">
             <div className={`${fullWidth ? "" : "max-w-7xl"} mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center`}>
                 <Logo title />
-                <Search className="max-w-3xl" />
                 <div className="flex items-center space-x-4">
+                    <Search className="max-w-3xl" />
                     <DarkModeToggle />
                 </div>
             </div>
