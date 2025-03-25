b35ba2382372c3b65fe856524a9eb71676103e17
Phenixis <maxime.duhamel@etudiant.univ-rennes.fr>
2025-03-25 13:44:31 +0100

    add data for globe component with locations and associated IDs

--- app/globe/page.tsx
+++ app/globe/page.tsx
@@ -7,5 +7,18 @@ const GlobeComponent = dynamic(() => import("@/components/big/globe"), {
 });
 
 export default function Page() {
-  return <GlobeComponent className="min-h-screen min-w-screen" />;
+  return <GlobeComponent className="min-h-screen min-w-screen" data={[
+    {
+      lat: 48.8566,
+      lng: 2.3522,
+      name: "Paris",
+      ids: [1522, 1723],
+    },
+    {
+      lat: 40.7128,
+      lng: -74.006,
+      name: "New York",
+      ids: [1535, 1536, 1849],
+    }
+  ]} />;
 }
